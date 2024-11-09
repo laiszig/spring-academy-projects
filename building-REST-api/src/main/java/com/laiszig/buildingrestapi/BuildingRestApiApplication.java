5bb8985c30b82ff406f3c7988414a2b9ccc1eb83
laiszig <laisziegler@gmail.com>
2024-11-09 11:08:25 -0300

    new project

---
+++ building-REST-api/src/main/java/com/laiszig/buildingrestapi/BuildingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.buildingrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingRestApiApplication.class, args);
+    }
+
+}
---
+++ building-REST-api/src/test/java/com/laiszig/buildingrestapi/BuildingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.buildingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
